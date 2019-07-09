a76f98c70fd86766729698f12d5ff95d68285c5a
justiny2c <justiny2c@gmail.com>
2019-07-09 16:04:57 -0700

    Forgot to add PUT

--- posts-router.js
+++ posts-router.js
@@ -122,4 +122,26 @@ router.delete("/:id", (req,res) => {
      })
 })
 
+router.put("/:id", (req, res) => {
+    const id = req.params.id
+    const post = req.body
+
+    if(!id){
+        res.status(404).json({ message: "The post with the specified ID does not exist." })
+    } else {
+    db
+    .update(id, post)
+    .then(post => {
+        if(post.title && post.contents){
+            res.status(200).json(post)
+        } else {
+            res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
+        }
+    })
+    .catch(error =>{
+        res.status(500).json({ error: "The post information could not be modified." });
+    })
+    }
+})
+
 module.exports = router;
